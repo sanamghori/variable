23b75c02fbf684ee03f5f6dc6ad9af250972feeb
sanamghori <161497237+sanamghori@users.noreply.github.com>
2024-06-04 20:58:06 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,62 @@
+let colour:string="blue";
+colour="pink";
+console.log(colour)
+let fruit:number=30
+console.log(fruit)
+let present_Student:boolean=true
+console.log(present_Student)
+let fristName:string='anaya';
+let secondName:string='khan';
+let lastName:string=fristName+'  '+secondName;
+console.log(lastName)
+let fristclass:string='maiez';
+let secondclass:string='khan';
+let fullclass:string=`${fristclass} ${secondclass}`;
+console.log(fullclass);
+let num1:number=4
+let num2:number=4
+let totalnum:number=(num1+num2)
+console.log(totalnum)
+let cart:number=8
+let cart2:number=3
+let totalnumber:number=(cart-cart2)
+console.log(totalnumber)
+let num3:number=6
+let num4:number=3
+let totalnum2:number=(6*3);
+console.log(totalnum2);
+let num5:number=10
+let num6:number=2
+let totalnum3:number=(10/2);
+console.log(totalnum3)
+let leyer:number=5
+let apples:number=8
+let power:number= apples**2
+console.log(power)
+let one:number=10
+let two:number=3
+let three:number=one%two
+console.log(three)
+let A=6
+console.log(++A)
+let B=10
+console.log(--B)
+let C=5;
+let D=2;
+let E= ++C + C++ + --D + D-- + C + D++ +D
+console.log(E)
+let F=8
+console.log(F--)
+let G=10
+console.log(--G)
+let result=2+4*5
+console.log(result)
+let aa=5
+let bb=5
+console.log(aa==bb)
+let cc=8
+let dd=9
+console.log(cc==dd)
+for (let i=1;i<=5;i++){
+    console.log(i);
+}
